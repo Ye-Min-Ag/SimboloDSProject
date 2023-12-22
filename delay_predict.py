f22742c1993891dce97fc89bd0db4e1c4541d192
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-12-23 02:22:11 +0630

    Update delay_predict.py

--- delay_predict.py
+++ delay_predict.py
@@ -76,7 +76,7 @@ if st.button("Submit"):
     data_D1=pd.get_dummies(df)
     data_D2 =  data_D1.reindex(columns = dum3.columns, fill_value=0)
     y_preds = model.predict(data_D2)
-    st.write('Is your flight going to be delayed?')
+    st.title('Is your flight going to be delayed?')
     st.write('(1 means delay)')
     st.write(y_preds)
 
