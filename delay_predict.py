a46ac7ed8898319597f43f2eae29f975cbd8cf20
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-12-23 01:26:04 +0630

    Update delay_predict.py

--- delay_predict.py
+++ delay_predict.py
@@ -27,16 +27,30 @@ day_of_week = st.number_input("Day of Week in number", min_value = 1, step = 1)
 time = st.number_input("Time",min_value=0, step=1) 
 length = st.number_input("Length", min_value=1, step=1) 
 
+st.write("**Entered Information:**")
+st.write("Airline Name:", airline_name)
+st.write("Flight Number:", flight_number)
+st.write("Airport From:", airport_from)
+st.write("Airport To:", airport_to)
+st.write("Day of Week:", day_of_week)
+st.write("Time:", time)
+st.write("Length:", length)
 # Access the entered values
 if st.button("Submit"):
-    st.write("**Entered Information:**")
-    st.write("Airline Name:", airline_name)
-    st.write("Flight Number:", flight_number)
-    st.write("Airport From:", airport_from)
-    st.write("Airport To:", airport_to)
-    st.write("Day of Week:", day_of_week)
-    st.write("Time:", time)
-    st.write("Length:", length)
+    # Create a dictionary to hold the input data
+    data0 = {
+        "Airline Name": airline_name,
+        "Flight Number": int(flight_number),
+        "Airport From": airport_from,
+        "Airport To": airport_to,
+        "Day of Week": int(day_of_week),
+        "Time": int(time),
+        "Length": int(length) 
+    }
 
-    # Process the input data further based on your application's needs
+    # Create a DataFrame from the dictionary
+    df = pd.DataFrame(data)  # Create a single-row DataFrame
+    st.write(df)  # Display the DataFrame
+
+    # Further process the DataFrame as needed
 
