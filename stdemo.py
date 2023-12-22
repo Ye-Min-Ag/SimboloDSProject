e7428a43cf6cadf8158e9ec2baadc072d42f19e8
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-12-22 18:39:20 +0630

    Update stdemo.py

--- stdemo.py
+++ stdemo.py
@@ -13,8 +13,8 @@ from folium import plugins
 from streamlit.components.v1 import html
 
 st.title("Myanmar's 2012 Civil Aviation Analysis")
-html("my_map.html")
+html("https://github.com/Ye-Min-Ag/SimboloDSProject/raw/main/my_map.html")
 st.title("Myanmar Airports")
-html("my_map1.html")
+html("https://github.com/Ye-Min-Ag/SimboloDSProject/raw/main/my_map1.html")
 st.markdown("Myanmar Flight Routes")  # For a large title
 st.markdown("**Some Source airports are marked with green color.**")  # For a smaller descriptive text
