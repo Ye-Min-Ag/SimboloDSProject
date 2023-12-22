1c6978faa49fc5aa638023b269fe3b06927c526a
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-12-22 23:54:07 +0630

    Update delay_predict.py

--- delay_predict.py
+++ delay_predict.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+rfmodel= st.sidebar.checkbox('Decision Trees')
+data = pd.read_csv(r'airlines2.csv')
+
 
